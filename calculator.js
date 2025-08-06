161c056946ce913c89a678273a4881f96d65d5b4
Roro9526 <144102289+Roro9526@users.noreply.github.com>
2025-08-06 10:00:50 +0200

    calculette

---
+++ calculator.js
@@ -0,0 +1,21 @@
+// calculator.js
+export default class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error("division par 0");
+    }
+    return a / b;
+  }
+}
---
+++ calculator.test.js
@@ -0,0 +1,49 @@
+// calculator.test.js
+import Calculator from "./calculator.js";
+
+describe("Calculator", () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new Calculator();
+  });
+
+  test("Addition", () => {
+    const result = calculator.add(2, 3);
+    expect(result).toBe(5);
+  });
+
+  test("Soustraction", () => {
+    const result = calculator.subtract(5, 3);
+    expect(result).toBe(2);
+  });
+
+  test("resultat negatif", () => {
+    const result = calculator.subtract(3, 5);
+    expect(result).toBe(-2);
+  });
+
+  test("Mutiplication", () => {
+    const result = calculator.multiply(4, 5);
+    expect(result).toBe(20);
+  });
+
+  test("0", () => {
+    expect(calculator.multiply(0, 123)).toBe(0);
+    expect(calculator.multiply(123, 0)).toBe(0);
+  });
+
+  test("Division", () => {
+    const result = calculator.divide(10, 2);
+    expect(result).toBe(5);
+  });
+
+  test("Fraction", () => {
+    const result = calculator.divide(7, 2);
+    expect(result).toBe(3.5);
+  });
+
+  test("erreur division 0", () => {
+    expect(() => calculator.divide(5, 0)).toThrow("pas de division par 0");
+  });
+});
